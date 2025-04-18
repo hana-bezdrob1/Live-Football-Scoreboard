cb1dcaab2bd58529a98c7e0424e15bf67ca04391
hana-bezdrob1 <hana.bezdrob1997@gmail.com>
2025-04-18 10:38:31 +0200

    Implement check for negative scores

--- scoreboard/src/main/java/com/hanabezdrob/scoreboard/Score.java
+++ scoreboard/src/main/java/com/hanabezdrob/scoreboard/Score.java
@@ -1,6 +1,12 @@
 package com.hanabezdrob.scoreboard;
 
 public record Score(int home, int away) {
+    public Score {
+        if (home < 0 || away < 0) {
+            throw new IllegalArgumentException("Scores may not be negative.");
+        }
+    }
+
     public int total() {
         return home + away;
     }
