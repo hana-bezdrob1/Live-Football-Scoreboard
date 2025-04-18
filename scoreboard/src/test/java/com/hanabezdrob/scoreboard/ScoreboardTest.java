ebce66b7657b65bec3502dcf8d1a9e29cfd799a4
hana-bezdrob1 <hana.bezdrob1997@gmail.com>
2025-04-18 09:59:25 +0200

    Add test for finishing match

--- scoreboard/src/test/java/com/hanabezdrob/scoreboard/ScoreboardTest.java
+++ scoreboard/src/test/java/com/hanabezdrob/scoreboard/ScoreboardTest.java
@@ -2,6 +2,8 @@ package com.hanabezdrob.scoreboard;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ScoreboardTest {
@@ -31,5 +33,24 @@ public class ScoreboardTest {
         final Match updatedMatch = scoreboard.getMatchesInProgress().getFirst();
         assertThat(updatedMatch.getHomeScore()).isEqualTo(1);
         assertThat(updatedMatch.getAwayScore()).isEqualTo(0);
+    }
+
+    @Test
+    void finishMatch_shouldRemoveMatchFromScoreboard() {
+        final Scoreboard scoreboard = new Scoreboard();
+        final Match firstMatch = scoreboard.startMatch("Bosnia and Herzegovina", "Norway");
+        final Match secondMatch = scoreboard.startMatch("Spain", "Mexico");
+
+        assertThat(scoreboard.getMatchesInProgress()).hasSize(2);
+
+        scoreboard.finishMatch(firstMatch);
+
+        final List<Match> remainingMatches = scoreboard.getMatchesInProgress();
+        assertThat(remainingMatches).hasSize(1);
+        assertThat(remainingMatches.getFirst()).isSameAs(secondMatch);
+
+        // Ensure that trying to complete a non-existent match will not do anything.
+        scoreboard.finishMatch(firstMatch);
+        assertThat(scoreboard.getMatchesInProgress()).hasSize(1);
     }
 }
